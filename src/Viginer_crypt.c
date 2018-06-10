8d71fc2f1655f9a8f385e9dfa4c08daa5f740da3
Sergey <grafe2210@localhost.localdomain>
2018-06-10 18:10:14 +0700

    fix conditionsfor crypt and uncrypt

--- src/Viginer_crypt.c
+++ src/Viginer_crypt.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include <Viginer.h>
+#include "Viginer.h"
 
-char viginer_in(char *string, char f, short lang)
+char viginer_in(char string, char f, short lang)
 {
     char new_ssq; //переменная для шифрования
 
     //условия для шифрования
 
-    if((*string >= 'a' && *string <= 'z') || (*string >= 'A' && *string <= 'A') ||
-    (*string >= 'а' && *string <= 'я') || (*string >= 'А' && *string <= 'Я')) {
+    if ((string >= 'a' && string <= 'z') || (string >= 'A' && string <= 'Z') ||
+        (string >= 'а' && string <= 'я') || (string >= 'А' && string <= 'Я')) {
         new_ssq = *string + f;
         
         //исключение возможности выхода за пределы используемого алфавита
         
-        if((*string >= 'Z' && *string <= 'a') || (*string >= 'z' && *string <= 'А') ||
-        (*string >= 'я') {
-            if(lang == 1) {
+        if ((string >= 'Z' && string <= 'a') || (string >= 'z' && string <= 'А') ||
+            (string >= 'я') {
+            if (lang == 1) {
                 new_ssq = new_ssq - 26;
             } else {
                 new_ssq = new_ssq - 33;
--- src/Viginer_uncrypt.c
+++ src/Viginer_uncrypt.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <Viginer.h>
 
-char viginer_out(char *string, char f, short lang)
+char viginer_out(char string, char f, short lang)
 {
     char new_ssq; //переменная для дешифрования
 
     //условия для шифрования
 
-    if((*string >= 'a' && *string <= 'z') || (*string >= 'A' && *string <= 'A') ||
-    (*string >= 'а' && *string <= 'я') || (*string >= 'А' && *string <= 'Я')) {
-        new_ssq = *string - f;
+    if ((string >= 'a' && string <= 'z') || (string >= 'A' && string <= 'Z') ||
+            (string >= 'а' && string <= 'я') || (string >= 'А' && string <= 'Я')) {
+        new_ssq = string - f;
         
         //исключение возможности выхода за пределы используемого алфавита
         
-        if((*string >= 'Z' && *string <= 'a') || (*string >= 'z' && *string <= 'А') ||
-        (*string >= 'я') {
+        if ((string >= 'Z' && string <= 'a') || (string >= 'z' && string <= 'А') ||
+                (string >= 'я') {
             if(lang == 1) {
                 new_ssq = new_ssq + 26;
             } else {
