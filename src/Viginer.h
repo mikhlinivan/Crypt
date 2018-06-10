57489c7c5e31b62064f8237bfb9fc552d4d3f2b6
Sergey <grafe2210@localhost.localdomain>
2018-06-10 17:24:55 +0700

    create uncrypt function for Viginer

--- src/Viginer.h
+++ src/Viginer.h
@@ -1,3 +1,5 @@
 #include <stdio.h>
 
 char viginer_in(char *string, char f, short lang);
+
+char viginer_out(char *string, char f, short lang);
---
+++ src/Viginer_uncrypt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <Viginer.h>
+
+char viginer_out(char *string, char f, short lang)
+{
+    char new_ssq; //переменная для дешифрования
+
+    //условия для шифрования
+
+    if((*string >= 'a' && *string <= 'z') || (*string >= 'A' && *string <= 'A') ||
+    (*string >= 'а' && *string <= 'я') || (*string >= 'А' && *string <= 'Я')) {
+        new_ssq = *string - f;
+        
+        //исключение возможности выхода за пределы используемого алфавита
+        
+        if((*string >= 'Z' && *string <= 'a') || (*string >= 'z' && *string <= 'А') ||
+        (*string >= 'я') {
+            if(lang == 1) {
+                new_ssq = new_ssq + 26;
+            } else {
+                new_ssq = new_ssq + 33;
+            }
+        }
+    }
+    return new_ssq;
+}
