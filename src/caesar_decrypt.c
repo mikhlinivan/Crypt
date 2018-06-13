770cf05a89ae75d5d01d680de1c0864918cccba4
GachiGit <kirillefremov1999@gmail.com>
2018-06-13 16:06:21 +0700

    Fix caesar_decrypt.c

---
+++ src/caesar_decrypt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "caesar.h"
+#include <locale.h>
+setlocale(LC_ALL, "Rus");
+
+char caesar_out(char *string, int key) {
+    char new_ssq;
+    int key;
+
+    if ((*string >= 'a' && *string <= 'z') || (*string >= 'A' && *string <= 'Z') ||
+        (*string >= 'а' && *string <= 'я') || (*string >= 'А' && *string <= 'Я')) {
+                New_ssq = string - key;
+            }
+                if (string >= 'A' && string <='Z' && new_ssq > 'Z') {
+                    new_ssq = new_ssq + 26;
+                    }
+                else if (string >= 'a' && string <= 'z' && new_sqq > 'z') {
+                    new_ssq = new_ssq + 26;
+                    }
+                else if (string >= 'а' && string <='я' && new_ssq  > 'я') {
+                    new_ssq = new_ssq + 32;
+                    }
+                else if (string >= 'А' && string <= 'Я' && new_ssq > 'Я') {
+                    new_ssq = new_ssq + 32;
+                    }
+            }
+            return new_ssq;
+    }
+
+    return string;
+}
+
