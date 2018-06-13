7b23dd6f8e76240e502ecd7d8ab187e296edc998
mikhlinivan <SlenderNSK@gmail.com>
2018-06-13 17:53:41 +0700

    fix isue with setlocale

--- src/vernam_crypt.c
+++ src/vernam_crypt.c
@@ -1,13 +1,14 @@
 #include "stdio.h"
 #include "vernam_crypt.h"
 #include "locale.h"
-setlocale(LC_ALL, "Rus");
+
 
 char vernam (char string, char key)
 {
+	setlocale(LC_ALL, "Rus");
 	char crypt_string;//сивол куда будет идти шифрование на промежуточном этапе
 	int i; //счетчик для цикла
-	crypt_string = string
+	crypt_string = string;
 	if((string >= 'a' && string <= 'z') || (string >= 'A' && string <= 'Z')
 	|| (string >= 'а' && string <= 'я')||(string >= 'А' && string <= 'Я')) {
 		crypt_string = string ^ key;
