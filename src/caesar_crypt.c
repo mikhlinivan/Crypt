8a129f79283d084e9ab5398677f234b001414016
GachiGit <kirillefremov1999@gmail.com>
2018-06-13 22:55:17 +0700

    Fix5 caesar_crypt.c

--- src/caesar_crypt.c
+++ src/caesar_crypt.c
@@ -9,11 +9,11 @@ char caesar_in(char string, int key) {
     if ((string >= 'a' && string <= 'z') || (string >= 'A' && string <= 'Z') ||
 	(string >= 'а' && string <= 'я') || (string >= 'А' && string <= 'Я')) {
 		new_ssq = string + key;
-	    }
+
                 if (string >= 'A' && string <='Z' && new_ssq > 'Z') {
                     new_ssq = new_ssq - 26;
 		    }
-		else if (string >= 'a' && string <= 'z' && new_sqq > 'z') {
+		else if (string >= 'a' && string <= 'z' && new_ssq > 'z') {
                     new_ssq = new_ssq - 26;
                     }
 		else if (string >= 'а' && string <='я' && new_ssq  > 'я') {
