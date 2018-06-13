aed5bd384a87bc90737afd5ec5e6b7b37701b1b8
GachiGit <kirillefremov1999@gmail.com>
2018-06-13 17:34:25 +0700

    Correct2 caesar.h

--- src/caesar.h
+++ src/caesar.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-void caesar_crypt(char string, int key);
+char caesar_crypt(char string, int key);
 
-void caesar_decrypt(char string, int key);
+char caesar_decrypt(char string, int key);
