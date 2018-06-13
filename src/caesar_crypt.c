aa93372dd086326acdc7ce95e618c7d509d96cf7
GachiGit <kirillefremov1999@gmail.com>
2018-06-13 21:23:14 +0700

    Fix4 caesar_crypt.c

--- src/caesar_crypt.c
+++ src/caesar_crypt.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "caesar.h"
 #include <locale.h>
-setlocale(LC_ALL, "Rus");
 
 char caesar_in(char string, int key) {
+    setlocale(LC_ALL, "Rus");
     char new_ssq;
 
     if ((string >= 'a' && string <= 'z') || (string >= 'A' && string <= 'Z') ||
 	(string >= 'а' && string <= 'я') || (string >= 'А' && string <= 'Я')) {
-		New_ssq = string + key;
+		new_ssq = string + key;
 	    }
                 if (string >= 'A' && string <='Z' && new_ssq > 'Z') {
                     new_ssq = new_ssq - 26;
@@ -22,7 +22,6 @@ char caesar_in(char string, int key) {
 		else if (string >= 'А' && string <= 'Я' && new_ssq > 'Я') {
                     new_ssq = new_ssq - 32;
                     }
-            }
             return new_ssq;
     }
 
