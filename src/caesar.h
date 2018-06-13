0168057a6e429c0a2e2abab15d0e2aea18d0cd06
Sergey <grafe2210@localhost.localdomain>
2018-06-14 06:22:33 +0700

    little fix from max

--- src/caesar.h
+++ src/caesar.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-char caesar_crypt(char string, int key);
+char caesar_in(char string, int key);
 
-char caesar_decrypt(char string, int key);
+char caesar_out(char string, int key);
