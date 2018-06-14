7114a30ad8a3dca686fc480cc0eb06f6f9df0d66
mikhlinivan <SlenderNSK@gmail.com>
2018-06-14 08:00:11 +0700

    fix

--- src/vernam_decrypt.c
+++ src/vernam_decrypt.c
@@ -6,7 +6,7 @@
 char vernam_decrypt(char string, char key) {
 	setlocale(LC_ALL, "Rus");
 	char decrypted;
-	if((string ^ key >= 'a' && string^ key <= 'z') || (string ^ key >= 'A' && string ^ key <= 'Z')) {
+	if(((string ^ key) >= 'a' && (string ^ key) <= 'z') || ((string ^ key) >= 'A' && (string ^ key) <= 'Z')) {
 		decrypted = string ^ key;
 	}
 	else {
